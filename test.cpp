861b817d22269c146d872d534b40083589e737c3
hqx <130121049+hqxnb666@users.noreply.github.com>
2024-07-23 23:38:45 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,1 +1,24 @@
-edjoiasdjasldjaskljdlkashjfkihaslkvnkjsdhlfjlksfxcjmfgwolsmcvs
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s1, s2;
+    cin >> s1;
+    cin >> s2;
+    int arr[256] = { 0 };
+    for (char ch : s2) {
+        if (ch != ' ')
+        {
+            arr[ch] += 1;
+        }
+
+    }
+    for (int i = 0; i < s1.size(); i++) {
+        if (arr[s1[i]] > 0) {
+            s1.erase(i, 1);
+        }
+    }
+    
+    cout << s1 << endl;
+}
+// 64 Î»Êä³öÇëÓÃ printf("%lld")
