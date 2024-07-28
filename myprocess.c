dfa57f915a01f64131c79b63e3020b1da05271eb
hqxnb666 <2946635981@qq.com>
2024-07-29 01:50:47 +0800

    非阻塞等待

--- myprocess.c
+++ myprocess.c
@@ -23,19 +23,48 @@ int main()
     {
       ChildRun();
       printf("child quit..\n");
-      exit(0);
+      exit(123);
     }
-    sleep(5);
-    int status = 0;
-    pid_t rid = waitpid(id,&status,0);
-    if(rid >0){
-      printf("wait success,rid:%d\n",rid);
+    //father
+    while(1)
+    {
+      int status = 0;
+      pid_t rid = waitpid(id,&status,WNOHANG);
+      if(rid == 0)
+      {
+        usleep(1000);
+        printf("child is running, father check next time\n");
+
+      }
+      else if(rid >0)
+      {
+        if(WIFEXITED(status))
+        {
+          printf("child quit success, child exit code: %d\n",WEXITSTATUS(status));
+        }else{
+          if(WIFSIGNALED(status))
+          {
+            printf("child was killed by signedl,quit signal :%d\n",WTERMSIG(status));
+          }
+        }
+        break;
+      }
+      else{
+        printf("waitpid failed\n");
+        break;
+      }
     }
-    else{
-      printf("wait failed!\n");
-    }
-    sleep(3);
-    printf("father quit, status: %d, child quit code: %d, child quit signal: %d\n",status,(status >> 8)&0xFF, status&0x7F);
+   // sleep(5);
+    
+    //pid_t rid = waitpid(id,&status,0);
+    //if(rid >0){
+    //  printf("wait success,rid:%d\n",rid);
+    //}
+   // else{
+   //   printf("wait failed!\n");
+   // }
+   // sleep(3);
+   // printf("father quit, status: %d, child quit code: %d, child quit signal: %d\n",status,(status >> 8)&0xFF, status&0x7F);
 }
 
 
