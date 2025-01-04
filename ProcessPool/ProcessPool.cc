2bde597421c5bac96b3cf660d25ddb552afab9fe
hqxnb666 <2946635981@qq.com>
2025-01-05 00:18:59 +0800

    terminal processpool

--- ProcessPool/ProcessPool.cc
+++ ProcessPool/ProcessPool.cc
@@ -31,31 +31,27 @@ public:
     // & : 输入输出型参数
     // * : 输出型参数
     //  task_t task: 回调函数
-    void CreateChannelsandSubprocesses(int subprocessnum, std::vector<Channel>* channels,task_t task)
+    
+    void Wait()
     {
-        for(int i  = 0; i< subprocessnum; i++)
+        int status;
+        if (waitpid(_subprocesspid, &status, 0) == -1)
         {
-            int pipefd[2];
-            if(pipe(pipefd) == -1)
-            {
-                std::cerr << "pipe failed: " << strerror(errno) << std::endl;
-                exit(1);
-            }
-            pid_t id = fork();
-            if(id == -1)
-            {
-                std::cerr <<"fork failed:" << strerror(errno) << std::endl;
-            }
-            if(id == 0 )
-            {
-                if(i!= 0)
-                {
-                    //说明当前是第二个子进程，要
-                }
-            }
+            std::cerr << "waitpid failed: " << strerror(errno) << std::endl;
+        }
+        if(waitpid(_subprocesspid, &status, 0) > 0)
+        {
+            std::cout << "waitpid success" << std::endl;
+        }
+        if (WIFEXITED(status))
+        {
+            std::cout << _name << " exited with status " << WEXITSTATUS(status) << std::endl;
+        }
+        else
+        {
+            std::cerr << _name << " exited abnormally" << std::endl;
         }
     }
-
     void CloseChannel()
     {
         if (close(_wfd) == -1)
@@ -69,6 +65,90 @@ private:
     pid_t _subprocesspid;
     std::string _name;
 };
+ void CreateChannelsandSubprocesses(int subprocessnum, std::vector<Channel> *channels, task_t task)
+    {
+        for (int i = 0; i < subprocessnum; i++)
+        {
+            int pipefd[2];
+            if (pipe(pipefd) == -1)
+            {
+                std::cerr << "pipe failed: " << strerror(errno) << std::endl;
+                exit(1);
+            }
+            pid_t id = fork();
+            if (id == -1)
+            {
+                std::cerr << "fork failed:" << strerror(errno) << std::endl;
+            }
+            if (id == 0)
+            {
+                if (i != 0)
+                {
+                    // 说明当前是第二个子进程
+                    for (int j = 0; j < i; j++)
+                    {
+                        close((*channels)[j].getWfd());
+                    }
+                    
+                }
+                close(pipefd[1]);
+                dup2(pipefd[0], STDIN_FILENO);
+                    task();
+                    close(pipefd[0]);
+                    exit(0);
+
+            }
+            close(pipefd[0]);
+            channels->push_back(Channel(pipefd[1], id, "subprocess" + std::to_string(i)));
+
+        }
+    }
+    int NextChannelIndex(int size)
+    {
+        static int index = 0;
+        return index++ % size;
+    }
+    void SendTask(int wfd, int tasknum)
+    {
+        if (write(wfd, &tasknum, sizeof(tasknum)) == -1)
+        {
+            std::cerr << "write failed: " << strerror(errno) << std::endl;
+        }
+
+    }
+    void controlProcessonce(std::vector<Channel>& channels)
+    {
+        int tasknum = Selecttask();
+        int channel_index = NextChannelIndex(channels.size());
+        SendTask(channels[channel_index].getWfd(), tasknum);
+
+    }
+    void controlProcess(std::vector<Channel>& channels,int times = -1)
+    
+    {
+        if(times > 0)
+        {
+            while(times--)
+            {
+                controlProcessonce(channels);
+            }
+        }
+        else
+        {
+            while(1)
+            {
+                controlProcessonce(channels);
+            }
+        }
+    }
+     void cleanupchannels(std::vector<Channel>& channels)
+     {
+        for(auto& channel:channels)
+        {
+            channel.CloseChannel();
+            channel.Wait();
+        }
+     }
 int main(int argc, char *argv[])
 {
     if (argc < 2)
@@ -84,6 +164,10 @@ int main(int argc, char *argv[])
 
     std::vector<Channel> channels;
     // 1. create subprocessnum subprocesses and establish communication channels
-    CreateChannelsandSubprocesses(subprocessnum, channels);
+    CreateChannelsandSubprocesses(subprocessnum, &channels, work1);
+    // 2. control the subprocesses
+    controlProcess(channels, 10);
+    // 3. close the channels
+    cleanupchannels(channels);
     return 0;
 }
--- ProcessPool/task.hpp
+++ ProcessPool/task.hpp
@@ -7,7 +7,8 @@
 #include <vector>
 
 #define TaskNum 3
-typedef void(*task_t)(); //task_t is a function pointer type
+typedef void (*task_t)(); // task_t is a function pointer type
+task_t task[TaskNum];
 void task1()
 {
     std::cout << "task1" << std::endl;
@@ -20,8 +21,92 @@ void task3()
 {
     std::cout << "task3" << std::endl;
 }
+void ExcuteTask(int count)
+{
+    if (count >= 0 && count < TaskNum)
+    {
+        task[count]();
+    }
+    else
+    {
+        std::cerr << "task index out of range" << std::endl;
+        return;
+    }
+}
+void work()
+{
+    while (1)
+    {
+        int count = 0;
+        int n = read(0, &count, sizeof(count));
+        if (n == -1)
+        {
+            std::cerr << "read failed: " << strerror(errno) << std::endl;
+            exit(1);
+        }
+        if (n == sizeof(count))
+        {
+            std::cout << "pid is : " << getpid() << " handler task" << std::endl;
+            ExcuteTask(count);
+        }
+        if (n == 0)
+        {
+            std::cout << "read EOF" << std::endl;
+            exit(0);
+        }
+    }
+}
+void work1()
+{
+    while (1)
+    {
+        int count = 1;
+        int n = read(0, &count, sizeof(count));
+        if (n == -1)
+        {
+            std::cerr << "read failed: " << strerror(errno) << std::endl;
+            exit(1);
+        }
+        if (n == sizeof(count))
+        {
+            std::cout << "pid is : " << getpid() << " handler task" << std::endl;
+            ExcuteTask(count);
+        }
+        if (n == 0)
+        {
+            std::cout << "read EOF" << std::endl;
+            exit(0);
+        }
+    }
+}
+void work2()
+{
+    while (1)
+    {
+        int count = 2;
+        int n = read(0, &count, sizeof(count));
+        if (n == -1)
+        {
+            std::cerr << "read failed: " << strerror(errno) << std::endl;
+            exit(1);
+        }
+        if (n == sizeof(count))
+        {
+            std::cout << "pid is : " << getpid() << " handler task" << std::endl;
+            ExcuteTask(count);
+        }
+        if (n == 0)
+        {
+            std::cout << "read EOF" << std::endl;
+            exit(0);
+        }
+    }
+}  
+int Selecttask()
+    {
+        return rand() % TaskNum;
+    }
 
-task_t task[TaskNum] ;
 void Loadtask()
 {
     task[0] = task1;
